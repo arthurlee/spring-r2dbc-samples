60f00d768d234ba5a19bf743d1654c12d4a9881d
arthurlee <boyal2008@gmail.com>
2020-06-06 10:44:18 +0800

    create project

---
+++ src/main/java/com/boyal/demo/springr2dbcsamples/SpringR2dbcSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.boyal.demo.springr2dbcsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringR2dbcSamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boyal/demo/springr2dbcsamples/SpringR2dbcSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boyal.demo.springr2dbcsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
